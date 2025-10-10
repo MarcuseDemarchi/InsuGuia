2e2eff88432d409056e0f98f2ca349c03d55c70a
kalyo-silva <kalyo.silva@unidavi.edu.br>
2025-10-09 21:21:04 -0300

    feat: Desenvolvimento da Tela Inicial

--- insuguia/lib/main.dart
+++ insuguia/lib/main.dart
@@ -9,12 +9,59 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: ContinuousRectangleBorder(borderRadius: BorderRadiusGeometry.all(Radius.circular(10)))
+          )
+        )
+      ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('images/Logo.png'),
+                height: 200,
+              ),
+              ElevatedButton(
+                onPressed: press,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: Size(200, 32),
+                ),
+                child: Text(
+                  'Entrar',
+                  style: TextStyle(
+                    color: Colors.blue[700]
+                  ),
+                )
+              ),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                onPressed: press,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[700],
+                  fixedSize: Size(200, 32),
+                ),
+                child: Text(
+                  'Cadastrar-se',
+                  style: TextStyle(color: Colors.white),
+                )
+              ),
+            ],
+          ),
         ),
+        backgroundColor: Color.fromRGBO(13, 71, 161, 1),
       ),
     );
   }
+}
+
+
+void press(){
+  
 }
