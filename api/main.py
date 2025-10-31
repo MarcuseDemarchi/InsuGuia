70c088f221e4b9c2caf095572af810945eee886f
Marcus André Geacomo Demarchi <1031769@UNIDAVI.LOCAL>
2025-10-30 21:19:16 -0300

    feat(main) : adiciona end-points

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 from src.infra.database import Base,engine
+import src.infra.database.models
 
 Base.metadata.create_all(bind=engine)
 
@@ -7,15 +8,18 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return "Index page"
+    """ Function index do projeto """
+    return "Api esta funcionando corretamente"
 
-@app.post("/cadUser")
-def cadastra_usuário():
+@app.post("/validUser")
+def user_valid():
+    """ Valida o acesso do usuário """
     pass
 
-@app.get("/getUser")
-def hello_world():
-    return True
+@app.post("/cadUser")
+def user_register():
+    """ Cadastra o usuário dentro do sistema """
+    response = request.json()
 
 if __name__ == "__main__":
     app.run('127.0.0.1',5000)
