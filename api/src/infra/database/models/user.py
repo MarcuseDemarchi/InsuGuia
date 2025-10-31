fa458a1293aeff4a9ccb7c6d5b808fe945ba8ac0
Marcus André Geacomo Demarchi <1031769@UNIDAVI.LOCAL>
2025-10-30 21:18:50 -0300

    feat(infra) altera a estrutura da tabela user

--- api/src/infra/database/models/user.py
+++ api/src/infra/database/models/user.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, Integer, String
 from src.infra.database import Base
 
-class user(Base):
+class User(Base):
+    """ Tabela de usuário"""
     __tablename__ = "tbuser"
 
     usrcodigo = Column(Integer,primary_key=True,index=True,autoincrement=True)
     usrname = Column(String(length=15),nullable=False)
-    usrpassword = Column(String(length=45),nullable=False)
-    usremail = Column(String(length=45),nullable=False)
+    usrpassword = Column(String(length=255),nullable=False)
+    usremail = Column(String(length=45),nullable=False,unique=True)
+    usrsalt = Column(String,nullable=False,unique=True)
