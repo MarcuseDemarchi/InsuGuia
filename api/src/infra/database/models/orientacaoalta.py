0ccf99c7948d4f5f5e2a07bbc5b2c39006d2cdec
Marcus André Geacomo Demarchi <1031769@UNIDAVI.LOCAL>
2025-11-06 21:07:03 -0300

    feat: adiciona modelagens ao banco

---
+++ api/src/infra/database/models/orientacaoalta.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey,DateTime
+from sqlalchemy.sql import func
+from src.infra.database import Base
+
+class OrientacaoAlta(Base):
+    """ Tabela de orientacao para alta"""
+    __tablename__ = "tborientacaoalta"
+
+    precodigo = Column(Integer,primary_key=True,index=True,autoincrement=True)
+    procodigo = Column(Integer,ForeignKey("tbprotocolos.procodigo"),nullable=False)
+    prescricaocomple = Column(String,nullable=True)
+    predatacriacao = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False)
+    preatualizado = Column(
+        DateTime(timezone=True),        
+        onupdate=func.now(),
+        nullable=False)
---
+++ api/src/infra/database/models/protocolos.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String,Boolean, ForeignKey,DECIMAL
+from src.infra.database import Base
+
+class Protocolos(Base):
+    """ Tabela de protocolos médicos"""
+    __tablename__ = "tbprotocolos"
+
+    procodigo = Column(Integer,primary_key=True,index=True,autoincrement=True)
+
+    paccodigo = Column(Integer,ForeignKey("tbpaciente.paccodigo"),nullable=False)    
+    prodieta = Column(String(20),nullable=False)
+    prousocorticoide = Column(Boolean,nullable=False)
+    protipocorticoide = Column(String(20),nullable=False)
+    prodoencaepatica = Column(Boolean,default=False,nullable=False)
+    prosensibilidadeinsu = Column(Boolean,default=False,nullable=False)
+    protipoinsubasal = Column(String(20))
+    proescaladispositivo = Column(DECIMAL(1,2))
+    proposologiabasal = Column(String(50))
+    protipoinsulinarapida = Column(String(20))
+    prolimitebolusprandial = Column(Integer)
+    proclicemiaatual = Column(Integer,nullable=False)
+
+    progestante = Column(Boolean,default=False)
+    prosemanasgestantes = Column(Integer)
