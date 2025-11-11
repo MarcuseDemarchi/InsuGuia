93cf5366b8963c2d6159554682dca6f8d52307bd
MarcuseDemarchi <demarchimarcus2004@gmail.com>
2025-11-10 22:15:12 -0300

    refactor : ajusta nome de coluna

--- api/src/infra/database/models/user.py
+++ api/src/infra/database/models/user.py
@@ -6,7 +6,7 @@ class User(Base):
     __tablename__ = "tbuser"
 
     usrcodigo = Column(Integer,primary_key=True,index=True,autoincrement=True)
-    usrname = Column(String(length=15),nullable=False)
+    usrnamefull = Column(String(length=15),nullable=False)
     usrpassword = Column(String(length=255),nullable=False)
     usremail = Column(String(length=45),nullable=False,unique=True)
     usrsalt = Column(String,nullable=False,unique=True)
