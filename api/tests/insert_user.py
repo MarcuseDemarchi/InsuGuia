aa2a959a0af99d5822816d72cd4df3524dcd0995
MarcuseDemarchi <demarchimarcus2004@gmail.com>
2025-11-10 22:16:05 -0300

    feat : adiciona arquivos de teste para serem testados futuramente

---
+++ api/tests/insert_user.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "http://127.0.0.1:5000/cadUser"
+
+params = {
+    "user_name_full" : "teste_marcus",
+    "password" : "Teste@123",
+    "user_email" : "demarchimarcu112s20014@gmail.com"
+    }
+
+hearder = {"Content-Type": "application/json"}
+
+response = requests.post(url=url,json=params,
+                         verify=False,headers=hearder)
+print(response.text)
---
+++ api/tests/valid_user.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "http://127.0.0.1:5000/validUser"
+
+params = {
+    "user_email" : "demarchimarcu112s20014@gmail.com",
+    "password" : "Teste@123",
+    }
+
+hearder = {"Content-Type": "application/json"}
+
+response = requests.post(url=url,json=params,
+                         verify=False,headers=hearder)
+
+print(response.text)
